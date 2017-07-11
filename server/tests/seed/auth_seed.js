6ac49da23cad194f9b4de3f9b8f43be5e5851b17
johadi <jim.hadi10@gmail.com>
2017-07-11 16:46:05 +0100

    Implement the Sign up test cases

---
+++ server/tests/seed/auth_seed.js
@@ -0,0 +1,37 @@
+// auth_seed.js
+const User = require('./../../database/models').User;
+
+module.exports = {
+  emptyDB(done) {
+    User.destroy({ truncate: true })
+        .then((rows) => {
+          console.log(rows);
+          return done();
+        })
+        .catch(err => done(err));
+  },
+  setData(fullname, username, email, mobile, password, confirmPassword) {
+    return {
+      fullname,
+      username,
+      email,
+      mobile,
+      password,
+      confirm_password: confirmPassword
+    };
+  },
+  addUserToDb(done) {
+    User.create({
+      fullname: 'jimoh hadi',
+      username: 'ovenje',
+      email: 'ovenje@yahoo.com',
+      mobile: '8163041269',
+      password: '11223344' })
+        .then((user) => {
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+  }
+};

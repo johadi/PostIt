5591b4615ec9e88f448ad2a9f777c85e659bd0a0
johadi <jim.hadi10@gmail.com>
2017-07-11 11:37:40 +0100

    Refactor the helper methods to use json method instead of send method

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -1,19 +1,19 @@
 const handleError = (err, res) => {
   switch (err.code) {
     case 401:
-      return res.status(401).send(err.message);
+      return res.status(401).json(err.message);
     case 404:
-      return res.status(404).send(err.message);
+      return res.status(404).json(err.message);
     default:
-      return res.status(400).send(err);
+      return res.status(400).json(err);
   }
 };
 const handleSuccess = (code, body, res) => {
   switch (code) {
     case 201:
-      return res.status(201).send(body);
+      return res.status(201).json(body);
     default:
-      return res.status(200).send(body);
+      return res.status(200).json(body);
   }
 };
 module.exports = { handleError, handleSuccess };

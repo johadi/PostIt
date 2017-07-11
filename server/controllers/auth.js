13fd53cf2457434bd3cca2dd343e054defe34830
johadi <jim.hadi10@gmail.com>
2017-07-11 17:03:31 +0100

    Sign up controller implemented

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,17 +3,15 @@ const User = require('../database/models').User;
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 const Validator = require('validatorjs');
+const { handleError, handleSuccess } = require('../helpers/helpers');
 
 module.exports = {
-  index(req, res) {
-    res.send('update here');
-  },
   signup(req, res) {
     const obj = req.body;
     const validator = new Validator(obj, User.signupRules());
     if (validator.passes() && obj.confirm_password) {
       if (obj.confirm_password !== obj.password) {
-        return res.status(400).send({ message: 'password not matched' });
+        return handleError('password not matched', res);
       }
       User.findOne({
         where: {
@@ -26,7 +24,7 @@ module.exports = {
               if (existingUser.email === obj.email) message = 'A user with this email already exists';
               if (existingUser.mobile === obj.mobile) message = 'This Mobile Number has been used';
               if (existingUser.username === obj.username) message = 'This Username has been used';
-              return res.status(400).send({ message });
+              return Promise.reject(message);
             }
             // if user does not exist and he/she registering for the first time
             if (!req.body.mobile) {
@@ -34,7 +32,7 @@ module.exports = {
             }
             return User.create(obj, { fields: ['email', 'password', 'username', 'mobile', 'fullname'] });
           })
-          .then(savedUser => res.status(201).send(savedUser))
+          .then(savedUser => handleSuccess(201, savedUser, res))
           .catch(err => res.status(400).send(err));
     } else {
       return res.status(400).send({ message: 'There are problems with your input' });

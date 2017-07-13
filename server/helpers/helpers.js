b7b54400701e9c2ee0587a635d2b7fbfc2f554a4
johadi <jim.hadi10@gmail.com>
2017-07-13 22:29:11 +0100

    Change error handlers to objects

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -2,20 +2,20 @@
 const handleError = (err, res) => {
   switch (err.code) {
     case 401:
-      return res.status(401).json(err.message);
+      return res.status(401).json({ status: 401, message: err.message });
     case 404:
-      return res.status(404).json(err.message);
+      return res.status(404).json({ status: 404, message: err.message });
     default:
-      return res.status(400).json(err);
+      return res.status(400).json({ status: 400, message: err.message });
   }
 };
 // success message handler
 const handleSuccess = (code, body, res) => {
   switch (code) {
     case 201:
-      return res.status(201).json(body);
+      return res.status(201).json({ status: 201, data: body });
     default:
-      return res.status(200).json(body);
+      return res.status(200).json({ status: 200, data: body });
   }
 };
 module.exports = { handleError, handleSuccess };

be479e78cd82c23a7d13a680afe036cc06e027e1
johadi <jim.hadi10@gmail.com>
2017-07-22 14:19:20 +0100

    remove client files from server files

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, browserHistory } from 'react-router';
-import './build/static/styles/cover.css';
-import routes from './routes';
-
-ReactDOM.render(<Router history={browserHistory}>{routes}</Router>,
-document.getElementById('app'));

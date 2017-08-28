740988c9575d1f22372bff735b967efa9782ddd1
johadi10 <jim.hadi10@gmail.com>
2017-08-29 00:36:30 +0100

    Implement more e2e test suites

--- client/tests/e2e/indexPage.js
+++ client/tests/e2e/indexPage.js
@@ -1,30 +1,31 @@
-// import config from './config';
-//
-// export default {
-//   before: (browser) => {
-//     browser
-//       .url(config.baseUrl)
-//       .waitForElementVisible('body', 1000)
-//       .waitForElementVisible('form', 3000);
-//   },
-//   'INDEX PAGE: Signin button (Say user clicked singin button in home page)': (browser) => {
-//     browser
-//       .assert.elementPresent('.btn-success')
-//       .assert.containsText('.btn-success', 'Login now')
-//       .click('.btn-success')
-//       .pause(1000)
-//       .assert.urlEquals(`${config.baseUrl}/signin`)
-//       .end();
-//   },
-//   'INDEX PAGE: Signup button (Say user clicked Signup button in index page)': (browser) => {
-//     browser
-//       .url(config.baseUrl)
-//       .assert.elementPresent('.btn-danger')
-//       .assert.containsText('.btn-danger', 'Join now')
-//       .click('.btn-danger')
-//       .pause(1000)
-//       .assert.urlEquals(`${config.baseUrl}/signup`)
-//       .end();
-//   },
-//   after: browser => browser.end()
-// };
+import config from './config';
+
+export default {
+  before: (browser) => {
+    browser
+      .url(config.baseUrl)
+      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('form', 3000);
+  },
+  'INDEX PAGE: Signin button (Say user clicked singin button in home page)': (browser) => {
+    browser
+      .url(config.baseUrl)
+      .assert.elementPresent('.btn-success')
+      .assert.containsText('.btn-success', 'Login now')
+      .click('.btn-success')
+      .pause(1000)
+      .assert.urlEquals(`${config.baseUrl}/signin`)
+      .end();
+  },
+  'INDEX PAGE: Signup button (Say user clicked Signup button in index page)': (browser) => {
+    browser
+      .url(config.baseUrl)
+      .assert.elementPresent('.btn-danger')
+      .assert.containsText('.btn-danger', 'Join now')
+      .click('.btn-danger')
+      .pause(1000)
+      .assert.urlEquals(`${config.baseUrl}/signup`)
+      .end();
+  },
+  after: browser => browser.end()
+};

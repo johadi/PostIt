bdf7b62fee487e64c6009a84efa6b7f91b32bbf6
johadi10 <jim.hadi10@gmail.com>
2017-10-08 00:58:45 +0100

    [Chore] Remove irrelevant script

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -1,23 +1,7 @@
 const fs = require('fs');
 const cheerio = require('cheerio');
 const colors = require('colors');
-const path = require('path');
 
-// const directory = 'production';
-// fs.readdir(directory, (err, files) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   files.forEach((file) => {
-//     if (file !== 'index.html') {
-//       fs.unlink(path.join(directory, file), err => {
-//         if (err) {
-//           console.log(err);
-//         }
-//       });
-//     }
-//   });
-// });
 fs.readFile('client/src/build/index.html', 'utf8', (err, markup) => {
   if (err) {
     return console.log(err);

7abb9b76e39c397e2949d4f13c72bd4c30a38dbc
johadi10 <jim.hadi10@gmail.com>
2017-10-14 23:42:52 +0100

    chore(feedback2): remove link from quick link and activities of side bars as stated in feedback

--- client/src/components/groups/SideBar.jsx
+++ client/src/components/groups/SideBar.jsx
@@ -16,7 +16,7 @@ class SideBar extends React.Component {
     return (
         <div className="main side-bar col-md-push-2 col-md-3 col-sm-12 col-xs-12 well">
           <p>
-            <p className="btn navy-header btn-lg btn-block">Quick Links</p>
+            <div className="btn navy-header btn-lg btn-block">Quick Links</div>
             <Link to="/create-group" className="btn btn-default btn-block">
               <i className="fa fa-lg fa-plus-circle text-display" aria-hidden="true">
               </i> Create Group

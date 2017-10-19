82f22c5fa6f9250cb2dc7da31e7225645efde778
johadi10 <jim.hadi10@gmail.com>
2017-10-19 23:31:05 +0100

    bug(feedback2):  Refactor the store to reflect feedback

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import reducers from '../reducers/index';
+import reducers from '../reducers';
 
 const store = createStore(reducers, {}, applyMiddleware(thunk, logger));
 export default store;

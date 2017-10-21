f3d31bc3d81dde55b96b1e0b52bbb8b2922e18fd
johadi10 <jim.hadi10@gmail.com>
2017-10-21 13:40:31 +0100

    chore(feedback2): Add and abstract footer

---
+++ client/src/components/footers/MainFooter.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * Main footer component
+ * @function MainFooter
+ * @param {object} props
+ * @return {XML} JSX
+ */
+const MainFooter = props =>
+  <div className="row footer-div">
+      <footer className="text-center text-display">
+        <p>Copyright &copy; Johadi PostIt 2017</p>
+      </footer>
+    </div>;
+export default MainFooter;
---
+++ client/src/components/footers/index.jsx
@@ -0,0 +1,7 @@
+import MainFooter from './MainFooter.jsx';
+import IndexFooter from './IndexFooter.jsx';
+
+export {
+  MainFooter,
+  IndexFooter
+};

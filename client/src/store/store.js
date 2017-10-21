c4df29dcaf9402cccd9de71b6ccd4528f67709d4
johadi10 <jim.hadi10@gmail.com>
2017-10-22 00:28:34 +0100

    chore(feedback2): Remove redux logger

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import reducers from '../reducers';
 
-const store = createStore(reducers, {}, applyMiddleware(thunk, logger));
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 export default store;
 

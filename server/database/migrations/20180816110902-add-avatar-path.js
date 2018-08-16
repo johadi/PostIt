5c254ea0fc3586def00fef2d316151a7883342ae
johadi <jimoh.hadi@gmail.com>
2018-08-16 12:14:18 +0100

    chore(users): add avatar column to the users database table

---
+++ server/database/migrations/20180816110902-add-avatar-path.js
@@ -0,0 +1,24 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+     */
+    return queryInterface.addColumn('Users', 'avatar_path', {
+      allowNull: true,
+      type: Sequelize.STRING
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+    */
+    return queryInterface.removeColumn('Users', 'avatar_path');
+  }
+};
